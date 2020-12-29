b4e0185f02b78839c9b15441372bac41d584fcc8
hoang4a <74579780+hoang4a@users.noreply.github.com>
2020-12-29 23:28:53 +0700

    updateloi

--- app.js
+++ app.js
@@ -24,21 +24,54 @@ app.get("/insert", (req, res) => {
 });
 app.post("/doInsert", async (req, res) => {
   let nameInput = req.body.txtName;
+
   let priceInput = req.body.txtPrice;
   let amoutInput = req.body.txtAmout;
   let staffInput = req.body.txtStaff;
-
-  let newProduct = {
-    productName: nameInput,
-    price: priceInput,
-    amout: amoutInput,
-    StaffName: staffInput,
+  let cutStr = nameInput.substring(1);
+  let staffStr = staffInput.substring(1);
+  let errorMsg = {
+    name: "",
+    price: "",
+    staff: "",
   };
-  let client = await MongoClient.connect(url);
-  let dbo = client.db("ProductDB");
-  await dbo.collection("icream").insertOne(newProduct);
-  res.redirect("/");
+  if (nameInput != null && nameInput.length < 5) {
+    errorMsg.name = "Name's length >=5";
+    console.log(nameInput);
+  }
+  if (priceInput != null && eval(priceInput) < 5) {
+    errorMsg.price = "Price must >=0";
+  }
+  if (errorMsg.name.length != 0 || errorMsg.price.length) {
+    res.render("insert", { error: errorMsg });
+  }
+  if (staffInput != null && staffInput.length < 5) {
+    errorMsg.staff = "Staff must >=10";
+  }
+  if (checkiput(cutStr) === false || checkiput(staffStr) === false) {
+    /// check viet hoa
+    errorMsg.name = "ki tu viet hoa";
+    errorMsg.staff = "ki tu staff phai viet hoa";
+  } else {
+    let newProduct = {
+      productName: nameInput,
+      price: priceInput,
+      amout: amoutInput,
+      StaffName: staffInput,
+    };
+    let client = await MongoClient.connect(url);
+    let dbo = client.db("ProductDB");
+    await dbo.collection("icream").insertOne(newProduct);
+    res.redirect("/");
+  }
 });
+function checkiput(str) {
+  // Check viet hoa
+  if (str === str.toUpperCase()) {
+    console.log("Check chu hoa");
+    return true;
+  } else return false;
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
